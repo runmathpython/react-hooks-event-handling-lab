20f6592a6b46ef684dda1a7e08b131851e56053e
Seong Kim <runmath@gmail.com>
2023-06-04 21:31:04 -0600

    tested but failed some

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleClick(eye){
+      if(eye === "focus"){
+        console.log("Good!")
+      } else if(eye === "blur"){
+          console.log("Hey! Eyes on me!")
+      }
+    }
+  
+  return(
+      <div>
+        <button onClick={() => handleClick("focus")} onMouseOver={() => handleClick("blur")}>Eyes on me</button>
+        
+      </div>
+    )
+  }
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function handleChange(event){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+          <input
+            type="password"
+            name="search"
+            onChange={handleChange}
+            placeholder="Enter password"
+        />
+        </div>
     )
 }
 
